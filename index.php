0bca852348b6b3dbed071a9f0eda28d747b877bc
jalanniroula <70659677+jalanniroula@users.noreply.github.com>
2020-11-15 18:09:48 -0500

    Add files via upload
    Add styles to front page

--- index.php
+++ index.php
@@ -4,15 +4,17 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="styles.css">
     <title> ViCE</title>
+    <div class="topnav">
+      <a class="active" href="index.php">Home</a>
+      <a href="contribute.html">Contribute</a>
+      <a href="about.html">About</a>
+      <form action="searchResults.php" method="post" >
+        <input type="text" name="valueToSearch" placeholder="Search Record.."></br>
+      </form>
+    </div>
   </head>
   <body>
-  <div class="topnav">
-    <a class="active" href="index.html">Home</a>
-    <a href="contribute.html">Contribute</a>
-    <a href="events.html">Events</a>
-    <a href="#about">About</a>
-    <input type="text" placeholder="Search..">
-  </div>
+
 
 <!---
 featured images/computers
@@ -21,37 +23,22 @@ featured images/computers
   <div class="images">
     <div class="row">
       <div class="column">
-          <img src="images/computer.png" alt="Computer">
+          <img src="/computer.png" alt="Computer">
           <h4>Computers</h4>
       </div>
       <div class="column">
-        <img src="images/laptop.jpg" alt="Laptop">
+        <img src="/laptop.jpg" alt="Laptop">
         <h4>Laptops</h4>
       </div>
       <div class="column">
-        <img src="images/peripherals.jpg" alt="Peripherals">
+        <img src="/peripherals.jpg" alt="Peripherals">
         <h4>Peripherals</h4>
       </div>
     </div>
   </div>
 </section>
+<section id="tables">
 <?php
-//   function Openconnnection ()
-//   {
-//     try{
-//     $serverName="JALAN-8\\SQLEXPRESS01";
-//     $connectionInfo = array("Database"=>"db_ViceApplication");
-//     $conn = sqlsrv_connect($serverName, $connectionInfo);
-//     if($conn==false){
-//       die(print_r(sqlsrv_errors(),true));
-//   }
-// }
-//   catch(Exception $e)
-//   {
-//     echo "Error";
-//   }
-// }
-
 function ReadData()
 {
 
@@ -63,30 +50,31 @@ function ReadData()
           if($conn == FALSE)
               die(FormatErrors(sqlsrv_errors()));
           else {
-            echo"connected";
+            echo"_";
           }
       }
       catch(Exception $e)
       {
           echo("Error!");
       }
-
     try
       {
         $sql = "SELECT * FROM tbl_countries";
-        $getProducts = sqlsrv_query($conn, $sql);
-        if ($getProducts == FALSE)
+        $result = sqlsrv_query($conn,$sql);
+        if ($result == FALSE)
             die(print_r(sqlsrv_errors(),true));
-        echo "<tr>";
-        while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
-        {
-            echo"<br>";
-            echo"<th>";
-            print join ($row," ");
-            echo "</th>";
-        }
-        echo "</tr>";
-        sqlsrv_free_stmt($getProducts);
+            echo"<table>
+              <tr>
+                <th>Country_ID</th>
+                <th>Country_Code</th>
+                <th>Country_Name</th>
+              </tr>";
+          while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
+          {
+            echo "<tr><td>". $row["countries_id"] . "</td><td>" .$row["countries_ccode"]. "</td><td>". $row["countries_name"]. "</td><tr>";
+          }
+        echo"</table>";
+        sqlsrv_free_stmt($result);
         sqlsrv_close($conn);
       }
       catch(Exception $e)
@@ -94,34 +82,9 @@ function ReadData()
         echo("Error!");
       }
 }
-
-// function ReadData(){
-//   try{
-//     $serverName="JALAN-8\\SQLEXPRESS01";
-//     $connectionInfo = array("Database"=>"db_ViceApplication");
-//     $conn = sqlsrv_connect($serverName, $connectionInfo);
-//       $sql="select * from tbl_countries";
-//       $results = sqlsrv_query($conn, $sql);
-//       if($results==false)
-//       {
-//         die(print_r(sqlsrv_errors(),true));
-//       }
-//       echo "<table align='center'><tr><th>Country_ID</th><th>Country_Code</th><th>Country_Name</th></tr>";
-//       while ($row=sqlsrv_fetch_array($results,sqlsrv_fetch_assoc))
-//       {
-//         echo"<tr><td>" .$row["Country_ID"]. "</td><td>" . $row["Country_Code"]. "</td><td>" . $row["Country_Name"]. "</td></tr><br>";
-//         echo "<br>";
-//       }
-//       sqlsrv_close($conn);
-//     }
-//   catch(Exception $e)
-//   {
-//     echo "error";
-//   }
-// }
 ReadData();
-
 ?>
+</section>
   </body>
   <footer>
     In progress
